201ef681d5b66704c529c8b44962e6b74692c072
oleggud512 <oleggud52@gmail.com>
2026-01-27 20:57:20 +0100

    added eitherToOption

--- lib/src/fpdart.dart
+++ lib/src/fpdart.dart
@@ -4,3 +4,10 @@ import 'package:loadable/loadable.dart';
 extension FpdartExtensions<D, L> on Loadable<D, L> {
   Option<D> toOption() => isData ? some(data) : none();
 }
+
+extension FpdartEitherExtensions<D, L, E> on Loadable<Either<E, D>, L> {
+  Option<D> eitherToOption() => switch (this) {
+    LData(:final data) => data.toOption(),
+    _ => none(),
+  };
+}
