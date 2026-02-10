358f6e9972a86b9370a1260adcca364249e1cfa8
oleggud512 <oleggud52@gmail.com>
2026-02-10 19:20:24 +0100

    added fold and foldSimple

--- lib/src/fpdart.dart
+++ lib/src/fpdart.dart
@@ -15,4 +15,30 @@ extension FpdartEitherExtensions<D, L, E> on Loadable<Either<E, D>, L> {
     LData(:final data) => data.isRight(),
     _ => false,
   };
+
+  T foldSimple<T>({
+    required T Function(Either<E, D> data) initialOrLoading,
+    required T Function(E error) error,
+    required T Function(D data) data,
+  }) {
+    return switch (this) {
+      LInitial(:final data) || LLoading(:final data) => initialOrLoading(data),
+      LData(data: Left(:final value)) => error(value),
+      LData(data: Right(:final value)) => data(value),
+    };
+  }
+
+  T fold<T>({
+    required T Function(Either<E, D> data) initial,
+    required T Function(Either<E, D> data, L loading) loading,
+    required T Function(E error) error,
+    required T Function(D data) data,
+  }) {
+    return switch (this) {
+      LInitial(:final data) => initial(data),
+      LLoading(:final data, loading: final l) => loading(data, l),
+      LData(data: Left(:final value)) => error(value),
+      LData(data: Right(:final value)) => data(value),
+    };
+  }
 }
